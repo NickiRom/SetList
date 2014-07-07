026007d340fcd5f33878f822524ad5ab12c2e873
NickiRom <romano.nh@gmail.com>
2014-07-07 15:10:17 -0700

    changing auth token

--- app/NEVERRESETHEAD.py
+++ app/NEVERRESETHEAD.py
@@ -55,7 +55,7 @@ def setlist(beats_playlist):
 #--------------------------------------------------------------------
 def beatspl2tracks(beats_playlist):
     
-    access_token = '?access_token=mhc4ubcd22reqpju62t3wd6q'
+    access_token = '?access_token=bkp2qmcb8wcygcjcuet3rk4j'
     client_id = '&client_id=cu4dweftqe5nt2wcpukcvgqu'
     
     url = 'https://partner.api.beatsmusic.com/v1/api/playlists/' + beats_playlist + access_token
