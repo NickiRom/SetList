874dcb9d0e9886dac91e0da4ac3c878b0422999d
Ubuntu <ubuntu@ip-172-31-41-176.us-west-2.compute.internal>
2014-07-29 18:10:16 +0000

    auth token

--- app/NEVERRESETHEAD.py
+++ app/NEVERRESETHEAD.py
@@ -55,7 +55,7 @@ def setlist(beats_playlist):
 #--------------------------------------------------------------------
 def beatspl2tracks(beats_playlist):
     
-    access_token = '?access_token=mgsbzu9jm8qd92e8vb382jgx'
+    access_token = '?access_token=epwgr7m3465fuzpsrdyf845b'
     client_id = '&client_id=cu4dweftqe5nt2wcpukcvgqu'
     
     url = 'https://partner.api.beatsmusic.com/v1/api/playlists/' + beats_playlist + access_token
